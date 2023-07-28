b36a894be50adbfc8004f0adbcd6244f27e97a37
Nikolai Lønberg <niklonberg@gmail.com>
2023-07-28 20:53:33 +0200

    adjusted display container, add keypad & display references for event delegation

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,20 @@
 /* imports */
-import Calculator from "../services/calculator-service.js";
+import CalculatorService from "../services/calculator-service.js";
 
 /* class instantiation */
-const calc = new Calculator();
+const CalculatorService = new CalculatorService();
 
 /* references */
 
   /* display */
-  const displayCalc = document.querySelector(".display__calc");
+  const display = document.querySelector('.display');
+  
+  const displayLast = document.querySelector(".display__last");
   const displayCurrent = document.querySelector(".display__current");
   const displayUndo = document.querySelector(".display__undo");
   
   /* keypad */
   const keypad = document.querySelector(".keypad");
+
+/* main */
+
--- services/calculator-service.js
+++ services/calculator-service.js
@@ -1,4 +1,4 @@
-class Calculator {
+class CalculatorService {
   constructor() {
   }
 
